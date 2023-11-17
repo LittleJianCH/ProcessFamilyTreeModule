4745b6b3fd0c434e92cc0458c597259260abdf13
简 <2447746158@qq.com>
2023-11-17 13:36:28 +0800

    add tree_gen.sh for test

---
+++ tree_gen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+create_process_tree() {
+  local depth=$1
+  local width=$2
+
+  if [ $depth -eq 0 ]; then
+    return
+  fi
+
+  for i in $(seq 1 $width); do
+    create_process_tree $((depth - 1)) $width &
+    local child_pid=$!
+
+    echo "Parent PID: $$, Child PID: $child_pid, Depth: $depth"
+  done
+
+  sleep 10000000
+  echo "PID($$) ends"
+}
+
+tree_depth=5
+tree_width=3
+
+create_process_tree $tree_depth $tree_width
